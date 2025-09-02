6b27f62c635c9ee1144b2402b09ce42318c93dee
Amritesh <amriteshk778@gmail.com>
2025-09-02 13:18:55 +0530

    Initial Commit

---
+++ syGroupTask/syGroupTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  syGroupTaskApp.swift
+//  syGroupTask
+//
+//  Created by Amritesh Kumar on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct syGroupTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
